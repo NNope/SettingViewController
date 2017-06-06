458de44816b6d4b75d8667fd4675c4d52b1284ea
NNope <455067828@qq.com>
2017-06-06 22:00:45 +0800

    create
    create

---
+++ SettingViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SettingViewController
+//
+//  Created by 谈Xx on 2017/6/6.
+//  Copyright © 2017年 谈Xx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SettingViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SettingViewController
+//
+//  Created by 谈Xx on 2017/6/6.
+//  Copyright © 2017年 谈Xx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
